fd8fb1f3c6c044a8995173b1a850d9f61cc0d538
Gaurav Singh <gaurav13.singh@ril.com>
2020-08-17 18:56:03 +0530

    first commit

---
+++ src/main/kotlin/com/xcalibure/foodapp/MmApplication.kt
@@ -0,0 +1,11 @@
+package com.xcalibure.foodapp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MmApplication
+
+fun main(args: Array<String>) {
+    runApplication<MmApplication>(*args)
+}
---
+++ src/test/kotlin/com/xcalibure/foodapp/MmApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.xcalibure.foodapp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MmApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
